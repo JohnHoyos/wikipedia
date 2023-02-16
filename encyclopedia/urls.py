77804b1ea1086e8876bbfe4ef26df6651c38da42
John Hoyos <jbayron11222@gmail.com>
2023-02-16 13:42:53 -0500

    Firt commit

---
+++ encyclopedia/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+app_name = "encyclopedia"
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("random", views.random, name="random"),
+    path("newpage", views.newpage, name="newpage"),
+    path("find", views.find, name="find"),
+    path("<str:title>", views.entrypage, name="entrypage")
+    
+    
+]
---
+++ encyclopedia/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from markdown2 import Markdown
+from django import forms
+from . import util
+
+markdowner = Markdown()
+
+class NewForm(forms.Form):
+    form = forms.CharField(label="search")
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+def random(request):
+    return render(request, "encyclopedia/random.html")
+
+def newpage(request):
+    return render(request, "encyclopedia/newpage.html")
+
+def entrypage(request, title):
+    print(title)
+    data = util.get_entry(title)
+    return render(request, "encyclopedia/entrypage.html",{        
+        'entry': markdowner.convert(data),
+        "title": title
+    })
+
+def find(request):    
+    if request.method == "POST":
+        print(request.POST['search'])
+        data = util.get_entry(request.POST['search'])
+    return render(request, "encyclopedia/entrypage.html",{        
+        'entry': markdowner.convert(data),
+        "title": request.POST['search']
+    })
+   
